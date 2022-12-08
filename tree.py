54688c1c2813eb434ea51ad76d97f9fdff7028a4
Marcel Suter <marcel.suter@bzz.ch>
2022-12-08 10:46:27 +0100

    Update tree.py

--- tree.py
+++ tree.py
@@ -3,11 +3,7 @@ def make_tree():
     prints an Xmas tree
     :return: None
     """
-    height = int(input('Hoehe > '))
-    line = 1
-    while line <= height:
-        print('')
-        line += 1
+    pass
 
 
 if __name__ == '__main__':
