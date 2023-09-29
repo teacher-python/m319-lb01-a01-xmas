fa2544a905477547feef491dc5adba08e04af1a4
Marcel Suter <marcel.suter@bzz.ch>
2023-09-29 08:53:32 +0200

    optimized tests

--- tree_test.py
+++ tree_test.py
@@ -1,73 +1,98 @@
+import sys
+
+import pytest
+
 import tree as tree
 
 
-def test_A(monkeypatch, capsys):
+def test_A(find_step, monkeypatch, capsys):
     inputs = iter(['1'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     tree.make_tree()
-    captured = capsys.readouterr()
+    output = capsys.readouterr().out
 
-    if len(captured.out) >= 2 and captured.out[-2] == '|':
-        print('Step 4')
-        assert captured.out == '*\n|\n'
-    elif captured.out == '*\n':
-        print('Step 3')
-        assert captured.out == '*\n'
+    if find_step == 4:
+        assert output == '*\n|\n'
+    elif find_step == 3:
+        assert output == '*\n'
     else:
-        print('Step 1 & Step 2')
-        assert captured.out == "\n"
+        assert output == "\n"
 
 
-
-def test_B(monkeypatch, capsys):
+def test_B(find_step, monkeypatch, capsys):
     inputs = iter(['5'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     tree.make_tree()
     captured = capsys.readouterr()
-    if len(captured.out) >= 2 and  captured.out[-2] == '|':
-        print('Step 4')
+    if find_step == 4:
         assert captured.out == '....*\n...***\n..*****\n.*******\n*********\n....|\n'
-    elif captured.out[0:5] == '....*' and captured.out[-3:] == '.*\n':
-        print('Step 3')
+    elif find_step == 3:
         assert captured.out == '....*\n...***\n..*****\n.*******\n*********\n'
-    elif captured.out[0:5] == '....\n':
-        print('Step 2')
+    elif find_step == 2:
         assert captured.out == '....\n...\n..\n.\n\n'
     else:
-        print('Step 1')
         assert captured.out == "\n\n\n\n\n"
 
 
-def test_C(monkeypatch, capsys):
+def test_C(find_step, monkeypatch, capsys):
+    if find_step == 1:
+        pytest.skip('Skipping this test for step 1')
     inputs = iter(['3'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     tree.make_tree()
     captured = capsys.readouterr()
-    if len(captured.out) >= 2 and  captured.out[-2] == '|':
-        print('Step 4')
+    if find_step == 4:
         assert captured.out == '..*\n.***\n*****\n..|\n'
-    elif captured.out[0:3] == '..*':
-        print('Step 3')
+    elif find_step == 3:
         assert captured.out == '..*\n.***\n*****\n'
     else:
-        print('Step 2')
         assert captured.out == '..\n.\n\n'
 
-def test_D(monkeypatch, capsys):
+
+def test_D(find_step, monkeypatch, capsys):
+    if find_step <= 2:
+        pytest.skip('Skipping this test for step 1 & 2')
     inputs = iter(['4'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     tree.make_tree()
     captured = capsys.readouterr()
-    if len(captured.out) >= 2 and  captured.out[-2] == '|':
-        print('Step 4')
+    if find_step == 4:
         assert captured.out == '...*\n..***\n.*****\n*******\n...|\n'
     else:
-        print('Step 3')
         assert captured.out == '...*\n..***\n.*****\n*******\n'
 
-def test_E(monkeypatch, capsys):
+
+def test_E(find_step, monkeypatch, capsys):
+    if find_step < 4:
+        pytest.skip('Skipping this test for step 1-3')
     inputs = iter(['7'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     tree.make_tree()
     captured = capsys.readouterr()
     assert captured.out == '......*\n.....***\n....*****\n...*******\n..*********\n.***********\n*************\n......|\n'
+
+
+@pytest.fixture
+def find_step(monkeypatch, capsys):
+    inputs = iter(['3'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    tree.make_tree()
+    output = capsys.readouterr().out
+    if output == '':
+        step = 0
+        print('Your program doesn\'t produce any output. No tests possible', file=sys.stderr)
+        pytest.exit('Your program doesn\'t produce any output. No tests possible')
+    elif '*' in output and '|' in output:
+        step = 4
+    elif '*' in output:
+        step = 3
+    elif '.' in output:
+        step = 2
+    elif output[0] == '\n':
+        step = 1
+    else:
+        print('Cannot determine the step you are working on.', file=sys.stderr)
+        step = 4
+
+    print(f'Executing tests for step {step}', file=sys.stderr)
+    return step
